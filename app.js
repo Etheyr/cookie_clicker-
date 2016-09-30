09cf9b8e9bf368f2f0f889561ec0955cce33e506
axel <axel.bouillart@gmail.com>
2016-09-30 15:02:10 +0200

    cookie clicker copi carbonne

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 	//votre code
-
-
+	var counter =0 ;
+	$(".cake").click(function() {
+		console.log(counter++);
+		$('span').html(counter+1)
+		if (counter === 20){
+			alert("Arrête sion bientôt tu va pouvoir acheter une nouvelle souris")
+		}
+			if (counter === 40){
+			alert("c'est pas faux")
+		}
+			if (counter === 60){
+			alert("Ah mais nan je connais pas de mot la ")
+		}
+			if (counter === 80){
+			alert("ok")
+		}
+			if (counter === 100){
+			alert("perceval")
+		}
+	});	
 });
